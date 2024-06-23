529cb4be5c7c50f6474246345b86de1d02126814
Kamil Adamiec <kamiladamiec95@wp.pl>
2024-06-23 19:10:20 +0200

    change extract file

--- src/extract.py
+++ src/extract.py
@@ -3,15 +3,32 @@ from dotenv import load_dotenv
 import json
 import requests
 
+load_dotenv('secrets/.env')
+OPENWEATHER_KEY_API = os.environ['OPENWEATHER_API_KEY']
 
-def get_data_open_weather_API():
-    load_dotenv('secrets/.env')
-    openweather_key_api = os.environ['OPENWEATHER_API_KEY']
+def get_city_coordinates(city):   
     GEO_DIRECT_URL = 'http://api.openweathermap.org/geo/1.0/direct?'
-    url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat=50&lon=50&appid={openweather_key_api}"
-    url2 = f"{GEO_DIRECT_URL}q=Gdansk,PL&appid={openweather_key_api}"
-    r = requests.get(url2)
-    return json.loads(r.text)
+    url = f'{GEO_DIRECT_URL}q={city},PL&appid={OPENWEATHER_KEY_API}'
+    r = requests.get(url)
+    lat = json.loads(r.text)[0]['lat']
+    lon = json.loads(r.text)[0]['lon']
+    return lat, lon
 
+def get_cities_pollution():
+    AIR_POLLUTION_URL = 'http://api.openweathermap.org/data/2.5/air_pollution?'
+    f = open('../config.json') 
+    cities = json.load(f)['cities'] 
+    cities_cord = {}
 
-print(get_data_open_weather_API())
+    for city in cities:
+        lat, lon = get_city_coordinates(city)
+        cities_cord[city] = lat, lon
+
+    cities_pollution = {}
+    for city, city_cord in cities_cord.items():
+        r = requests.get(f"{AIR_POLLUTION_URL}lat={city_cord[0]}&lon={city_cord[1]}&appid={OPENWEATHER_KEY_API}")
+        cities_pollution[city] = json.loads(r.text)['list'][0]['components']
+        
+    return cities_pollution
+
+print(get_cities_pollution())
