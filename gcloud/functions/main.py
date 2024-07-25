f84c4d8eafe794b15fd2e5993cd6aa8f8d6db121
Kamil Adamiec <kamiladamiec95@wp.pl>
2024-07-25 19:22:06 +0200

    cloud

--- gcloud/functions/main.py
+++ gcloud/functions/main.py
@@ -7,6 +7,7 @@ import base64
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))  # add gcloud_functions
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # add gcloud
 from src.extract import get_cities_pollution_history
+from src.extract import get_cities_pollution_history
 
 
 # PLIK CONFIG
@@ -18,4 +19,10 @@ def gcloud_get_openweather_data_function(request, context=None) -> str:
     pollution = get_cities_pollution_history('config.json')
     return str(pollution)
 
+
+
+@functions_framework.cloud_event
+def hello_pubsub(cloud_event):
+    # Print out the data from Pub/Sub, to prove that it worked
+    print(base64.b64decode(cloud_event.data["message"]["data"]))
 
