bc2c629a5e50fad241ca5afe20859bb1f6eed133
Kamil Adamiec <kamiladamiec95@wp.pl>
2024-08-08 18:49:09 +0200

    cloud

--- gcloud/functions/main.py
+++ gcloud/functions/main.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))  # add gcloud_functi
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # add gcloud
 from src.extract import get_cities_pollution_history
 from src.extract import get_cities_pollution_history
+import src.transform
 
 
 # PLIK CONFIG
@@ -24,5 +25,6 @@ def gcloud_get_openweather_data_function(request, context=None) -> str:
 @functions_framework.cloud_event
 def hello_pubsub(cloud_event):
     # Print out the data from Pub/Sub, to prove that it worked
-    print(base64.b64decode(cloud_event.data["message"]["data"]))
+    data = src.transform.convert_pandas(base64.b64decode(cloud_event.data["message"]["data"]))
+    return str(True)
 
--- gcloud/functions/src/transform.py
+++ gcloud/functions/src/transform.py
@@ -1,46 +1,55 @@
-from google.cloud import pubsub_v1
-import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\kamil\Desktop\de\AirPollutionApp\secrets\shining-rampart-426818-e1-b7a4fe01e204.json"
+import json
+import pandas as pd
+
+def convert_pandas(data):
+    data = json.loads(data)
+    data = pd.read_json(data)
+    return data
 
 
-def create_pubsub_connection():
+# from google.cloud import pubsub_v1
+# import os
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\kamil\Desktop\de\AirPollutionApp\secrets\shining-rampart-426818-e1-b7a4fe01e204.json"
 
-    client = pubsub_v1.SubscriberClient()
-    project_id = "shining-rampart-426818-e1"
-    subscription_id = "AirPollutionTopic-sub"
 
-    # Create a Pub/Sub subscriber client
-    subscriber = pubsub_v1.SubscriberClient()
-    subscription_path = subscriber.subscription_path(project_id, subscription_id)
+# def create_pubsub_connection():
 
-    return subscription_path
+#     client = pubsub_v1.SubscriberClient()
+#     project_id = "shining-rampart-426818-e1"
+#     subscription_id = "AirPollutionTopic-sub"
 
-# Callback function to process and transform messages
-def callback(message):
-    try:
-        create_pubsub_connection()
+#     # Create a Pub/Sub subscriber client
+#     subscriber = pubsub_v1.SubscriberClient()
+#     subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
-        # Decode the message data
-        message_data = message.data.decode("utf-8")
+#     return subscription_path
+
+# # Callback function to process and transform messages
+# def callback(message):
+#     try:
+#         create_pubsub_connection()
+
+#         # Decode the message data
+#         message_data = message.data.decode("utf-8")
         
-        # Transform the message (example: convert to uppercase)
-        transformed_message = message_data.upper()
+#         # Transform the message (example: convert to uppercase)
+#         transformed_message = message_data.upper()
         
-        print(f"Received message: {message_data}")
-        print(f"Transformed message: {transformed_message}")
+#         print(f"Received message: {message_data}")
+#         print(f"Transformed message: {transformed_message}")
         
-        # Acknowledge the message
-        message.ack()
-    except Exception as e:
-        print(f"Error processing message: {e}")
-        message.nack()
+#         # Acknowledge the message
+#         message.ack()
+#     except Exception as e:
+#         print(f"Error processing message: {e}")
+#         message.nack()
 
-# Subscribe to the subscription and listen for messages
-streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-print(f"Listening for messages on {subscription_path}...")
+# # Subscribe to the subscription and listen for messages
+# streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+# print(f"Listening for messages on {subscription_path}...")
 
-# Block the main thread indefinitely to keep the subscriber running
-try:
-    streaming_pull_future.result()
-except TimeoutError:
-    streaming_pull_future.cancel()
+# # Block the main thread indefinitely to keep the subscriber running
+# try:
+#     streaming_pull_future.result()
+# except TimeoutError:
+#     streaming_pull_future.cancel()
